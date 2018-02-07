c77e8f73fd34c611ea1c2b30d2985b7e28998533
HarrisZhang <hc_zhang@Ctrip.com>
2018-02-07 17:23:04 +0800

    update

---
+++ Savory.Web.ECharts4.RunExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savory.Web.ECharts4.RunExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EChartsOption option = new EChartsOption();
+
+            var content = EChartsConvert.Serialize(option);
+
+            File.WriteAllText(@"F:\1.txt", content);
+        }
+    }
+}
---
+++ Savory.Web.ECharts4/EChartsConvert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savory.Web.ECharts4
+{
+    public class EChartsConvert
+    {
+        public static string Serialize(EChartsOption option)
+        {
+            return "ok";
+        }
+    }
+}
