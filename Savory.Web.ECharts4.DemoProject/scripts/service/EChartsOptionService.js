536689e1c1dcbd9feb09faee39820991209b4a27
HarrisZhang <hc_zhang@Ctrip.com>
2018-02-08 12:01:23 +0800

    update

--- Savory.Web.ECharts4.DemoProject/scripts/service/EChartsOptionService.js
+++ Savory.Web.ECharts4.DemoProject/scripts/service/EChartsOptionService.js
@@ -1,3 +1,16 @@
-﻿function EChartsOptionService() {
+﻿function EChartsOptionService($resource, $q) {
 
+    var resource = $resource("/EChartsOptionHandler.ashx");
+
+    return {
+        firstexample: function () {
+            var d = $q.defer();
+            resource.save({ id: 'firstexample' }, null, function (result) {
+                d.resolve(result);
+            }, function (result) {
+                d.reject(result);
+            });
+            return d.promise;
+        }
+    }
 }
